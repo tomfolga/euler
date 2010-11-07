48c934460d6ef884a22d3a0dc7080034d7f379d7
Tom Folga <tomfolga@gmail.com>
2010-11-07 13:49:27 +0000

    Test

--- src/main/java/Problem9.scala
+++ src/main/java/Problem9.scala
@@ -2,10 +2,10 @@ object Problem9 {
 
   def main(args: Array[String]) {
     for (a <- 1 to 1000; b <- 1 to 1000; c <- 1 to 1000) {
-    	if (a + b +c == 1000 && (a * a + b *b == c *c )) {
-    		println (a*b*c)
-    	}
+      if (a + b + c == 1000 && (a * a + b * b == c * c)) {
+        println(a * b * c)
+      }
     }
-	  
+
   }
 }
